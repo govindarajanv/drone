b05a42c022367ae0f78b4a70892f001f492cdef2
Govindarajan V <govindarajanv@gmail.com>
2019-07-12 16:39:50 +0530

    Create Main.java

---
+++ src/main/java/com/govindarajanv/drone/java/Main.java
@@ -0,0 +1,13 @@
+package com.govindarajanv.drone.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+
+    public static String greet() {
+        return "Hi";
+    }
+
+}
